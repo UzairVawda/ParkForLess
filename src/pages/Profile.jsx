068d9a7a4551e0333694d54fd4541cc61d8da253
Uzair Vawda <uzair.vawda@gmail.com>
2023-01-23 20:52:00 -0500

    PFL: adding edit ability to profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,14 +1,18 @@
-import { getAuth } from "firebase/auth";
+import { db } from "../Firebase"
+import { getAuth, updateProfile } from "firebase/auth";
+import { updateDoc, doc } from "firebase/firestore";
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import { toast } from "react-toastify";
 
 export default function Profile() {
-	const auth = getAuth();
-  const navigate = useNavigate()
+  const auth = getAuth();
+  const navigate = useNavigate();
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
-    email: auth.currentUser.email
+    email: auth.currentUser.email,
   });
+  const [changeDetails, setChangeDetails] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -17,7 +21,28 @@ export default function Profile() {
       [name]: value,
     }));
   }
-	
+
+  function toggleChangeDetails() {
+    setChangeDetails((prevValue) => !prevValue);
+  }
+
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.displayName !== formData.name) {
+        await updateProfile(auth.currentUser, {
+          displayName: formData.name
+        })
+        const docRef = doc(db, "users", auth.currentUser.uid)
+        await updateDoc(docRef, {
+          name : formData.name
+        })
+        toast.success("Updated profile")
+      }
+    } catch (error) {
+      toast.error("Failed to update profile")
+    }
+  }
+
   function handleSignOut(event) {
     event.preventDefault();
     auth.signOut();
@@ -35,23 +60,39 @@ export default function Profile() {
               name="name"
               value={formData.name}
               onChange={handleChange}
-              disabled
-              className="w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-200 rounded-xl transition ease-in-out"
+              disabled={!changeDetails}
+              className={`w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-200 rounded-xl transition ease-in-out ${
+                changeDetails && "bg-red-100 focus:bg-red-10"
+              }`}
             />
-						<input
+            <input
               type="email"
               name="email"
               value={formData.email}
               onChange={handleChange}
               disabled
-              className="w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-200 rounded-xl transition ease-in-out"
+              className={'w-full px-4 py-2 mb-6 text-xl text-gray-700 bg-white border border-gray-200 rounded-xl transition ease-in-out'}
             />
-						<div className="mb-6 flex justify-between whitespace-nowrap text-sm sm:text-lg">
-							<p className="flex items-center">Do you want to change your name?
-								<span className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span>
-							</p>
-							<p onClick={handleSignOut} className="text-blue-600 hover:text-blue-700 transition ease-in-out duration-200 ml-1 cursor-pointer">Sign Out</p>
-						</div>
+            <div className="mb-6 flex justify-between whitespace-nowrap text-sm sm:text-lg">
+              <p className="flex items-center">
+                Do you want to change your name?
+                <span
+                  onClick={() => {
+                    changeDetails && onSubmit();
+                    toggleChangeDetails();
+                  }}
+                  className="text-red-600 hover:text-red-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+                >
+                  {changeDetails ? "Save" : "Edit"}
+                </span>
+              </p>
+              <p
+                onClick={handleSignOut}
+                className="text-blue-600 hover:text-blue-700 transition ease-in-out duration-200 ml-1 cursor-pointer"
+              >
+                Sign Out
+              </p>
+            </div>
           </form>
         </div>
       </section>
